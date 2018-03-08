62b4441f357c8d4560a304b78bd19184705377ec
Shelby <whit102@gmail.com>
2018-03-08 10:46:55 -0600

    added epicenter controller, feed, show

--- app/controllers/epicenter_controller.rb
+++ app/controllers/epicenter_controller.rb
@@ -1,6 +1,19 @@
 class EpicenterController < ApplicationController
   def feed
-  end
+    # @following_tweets = Tweet.where(user_id: current_user.following).or(user_id: User.find(1).id)
+      @following_tweets = []
+
+       # Tweet.all.each do |tweet|
+       # if current_user.following.include?(tweet.user_id)
+       #   || current_user.id == tweet.user_id
+       #   @following_tweets.push(tweet)
+       # end
+
+   # personal better equivalent of the above
+   array_of_users = current_user.following
+   array_of_users << current_user.id
+   @following_tweets - Tweet.where(user_id: array_of_users)
+end
 
   def show_user
   end
